fcc996bc81a11c0b951b85808ebc4827bf917f5b
Débora Andrade <deboraellenandrade@gmail.com>
2026-03-18 11:35:36 +0000

    feat: Remova rotas de tags de comentários não utilizadas

--- src/routes/commentRoutes.js
+++
@@ -1,9 +0,0 @@
-import { Router } from 'express';
-import { getComments, postComment } from '../controllers/commentController.js';
-
-const router = Router();
-
-router.get('/', getComments);
-router.post('/', postComment);
-
-export default router;
--- src/routes/taskTagRoutes.js
+++
@@ -1,9 +0,0 @@
-import { Router } from 'express';
-import { getTaskTags, postTaskTag } from '../controllers/tagController.js';
-
-const router = Router();
-
-router.get('/', getTaskTags);
-router.post('/', postTaskTag);
-
-export default router;
